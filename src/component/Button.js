8e69971f2ba4fa4d3be0aa0c19589309ad7a02c5
MohitAhir102 <ahirmohit120@gmail.com>
2023-12-18 22:59:22 +0530

    class add

--- src/component/Button.js
+++ src/component/Button.js
@@ -20,32 +20,32 @@ function TextControlsExample() {
     
       <div className="d-flex justify-content-center align-items-center vh-100">
         <div>
-          <Form.Control size="lg" type="text" className="caldisplay" placeholder="0" />
-          <br />
-          <Button variant="outline-secondary" value="AC" className="Btn1 ml-2 mb-2" onClick={btclick}>AC</Button>{" "}
-          <Button variant="outline-secondary" value="DE" className="Btn1 ml-2 mb-2" onClick={cleardisplay}>DE</Button>{" "}
-          <Button variant="outline-secondary" value="%" className="Btn1 ml-2 mb-2" >%</Button>{" "}
-          <Button variant="outline-secondary" value="/" className="Btn1 ml-2 mb-2" >/</Button>{" "} <br/>
+          <Form.Control size="lg" type="text" className="" placeholder="0" />
+          <br/>
+          <Button variant="btn btn-dark" value="AC" className="Btn1 ml-2 mb-2 text-dark" onClick={btclick}>AC</Button>{" "}
+          <Button variant="btn btn-dark" value="%" className="Btn1 ml-2 mb-2 text-dark" >%</Button>{" "}
+          <Button variant="btn btn-dark" value="7" className="Btn1 ml-2 mb-2 text-dark" >7</Button>{" "}
+          <Button variant="btn btn-dark" value="DE" className="Btn1 ml-2 mb-2 text-dark" onClick={cleardisplay}>DE</Button>{" "}<br/>
+          
+          <Button variant="btn btn-dark" value="/" className="Btn1 ml-2 mb-2 text-dark" >/</Button>{" "} 
+          <Button variant="btn btn-dark" value="8" className="Btn1 ml-2 mb-2 text-dark" >8</Button>{" "}
+          <Button variant="btn btn-dark" value="9" className="Btn1 ml-2 mb-2 text-dark" >9</Button>{" "}
+          <Button variant="btn btn-dark" value="*" className="Btn1 ml-2 mb-2 text-dark" >*</Button>{" "}<br/>
 
-          <Button variant="outline-secondary" value="7" className="Btn1 ml-2 mb-2" >7</Button>{" "}
-          <Button variant="outline-secondary" value="8" className="Btn1 ml-2 mb-2" >8</Button>{" "}
-          <Button variant="outline-secondary" value="9" className="Btn1 ml-2 mb-2" >9</Button>{" "}
-          <Button variant="outline-secondary" value="*" className="Btn1 ml-2 mb-2" >*</Button>{" "}<br/>
+          <Button variant="btn btn-dark" value="4" className="Btn1 ml-2 mb-2 text-dark" >4</Button>{" "}
+          <Button variant="btn btn-dark" value="5" className="Btn1 ml-2 mb-2 text-dark" >5</Button>{" "}
+          <Button variant="btn btn-dark" value="6" className="Btn1 ml-2 mb-2 text-dark" >6</Button>{" "}
+          <Button variant="btn btn-dark" value="-" className="Btn1 ml-2 mb-2 text-dark" >-</Button>{" "}<br/>
 
-          <Button variant="outline-secondary" value="4" className="Btn1 ml-2 mb-2" >4</Button>{" "}
-          <Button variant="outline-secondary" value="5" className="Btn1 ml-2 mb-2" >5</Button>{" "}
-          <Button variant="outline-secondary" value="6" className="Btn1 ml-2 mb-2" >6</Button>{" "}
-          <Button variant="outline-secondary" value="-" className="Btn1 ml-2 mb-2" >-</Button>{" "}<br/>
+          <Button variant="btn btn-dark" value="1" className="Btn1 ml-2 mb-2 text-dark" >1</Button>{" "}
+          <Button variant="btn btn-dark" value="2" className="Btn1 ml-2 mb-2 text-dark" >2</Button>{" "}
+          <Button variant="btn btn-dark" value="3" className="Btn1 ml-2 mb-2 text-dark" >3</Button>{" "}
+          <Button variant="btn btn-dark" value="+" className="Btn1 ml-2 mb-2 text-dark" >+</Button>{" "}<br/>
 
-          <Button variant="outline-secondary" value="1" className="Btn1 ml-2 mb-2" >1</Button>{" "}
-          <Button variant="outline-secondary" value="2" className="Btn1 ml-2 mb-2" >2</Button>{" "}
-          <Button variant="outline-secondary" value="3" className="Btn1 ml-2 mb-2" >3</Button>{" "}
-          <Button variant="outline-secondary" value="+" className="Btn1 ml-2 mb-2" >+</Button>{" "}<br/>
-
-          <Button variant="outline-secondary" value="00" className="Btn1 ml-2 mb-2" >00</Button>{" "}
-          <Button variant="outline-secondary" value="0" className="Btn1 ml-2 mb-2" >0</Button>{" "}
-          <Button variant="outline-secondary" value="." className="Btn1 ml-2 mb-2" >.</Button>{" "}
-          <Button variant="outline-secondary" value="=" className="Btn1 ml-2 mb-2" >=</Button>{" "}
+          <Button variant="btn btn-dark" value="00" className="Btn1 ml-2 mb-2 text-dark" >00</Button>{" "}
+          <Button variant="btn btn-dark" value="0" className="Btn1 ml-2 mb-2 text-dark" >0</Button>{" "}
+          <Button variant="btn btn-dark" value="." className="Btn1 ml-2 mb-2 text-dark" >.</Button>{" "}
+          <Button variant="btn btn-dark" value="=" className="Btn1 ml-2 mb-2 text-dark" >=</Button>{" "}
           
         </div>
       </div>
